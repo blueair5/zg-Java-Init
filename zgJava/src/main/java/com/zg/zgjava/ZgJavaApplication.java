35926b080fc37cdb4001e38ff72f407b3738fb42
张国伟 <1204378021@qq.com>
2024-09-09 13:35:19 +0800

    初始代码提交

---
+++ zgJava/src/main/java/com/zg/zgjava/ZgJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zg.zgjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZgJavaApplication.class, args);
+	}
+
+}
